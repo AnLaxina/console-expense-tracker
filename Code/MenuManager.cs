3f7048770f2d6239644741f70c953d98748729ba
Anilov Laxina <78817046+AnLaxina@users.noreply.github.com>
2025-03-09 16:13:35 -0700

    Add menu related methods from Program.cs to MenuManager.cs
    This allows for a more clean slate

---
+++ Code/MenuManager.cs
@@ -0,0 +1,50 @@
+﻿namespace console_expense_tracker.Code;
+
+public static class MenuManager
+{
+    internal static void ShowMenu()
+    {
+        Console.WriteLine("------------------------------------------------------------");
+        Console.WriteLine("Welcome to the Expense Tracker! Please type ONE option below: ");
+        Console.WriteLine("------------------------------------------------------------");
+        Console.WriteLine("1. Adds expense where you enter the amount, category, description, and date (optional: uses current date if not provided)");
+        Console.WriteLine("2. View Expense");
+        Console.WriteLine("3. Filter Expenses by date or category");
+        Console.WriteLine("4. Calculate Total Expenses");
+        Console.WriteLine("5. Close the program");
+        Console.WriteLine("------------------------------------------------------------");
+        Console.Write("Chosen option (enter a number): ");
+    }
+    
+    /// <summary>
+    ///  This method changes the menu based on what number the user inputs.
+    /// </summary>
+    /// <example>If the user selects 1, the console menu changes to show a menu that creates a new expense</example>
+    /// <param name="chosenOption">Based off the global variable _chosenOption</param>
+    internal static void ChangeMenu(int chosenOption)
+    {
+        switch (chosenOption)
+        {
+            case 1:
+                // TODO: Implement adding expense
+                Console.WriteLine("You chose 1!");
+                break;
+            case 2:
+                // TODO: Implement the ability to view expense
+                Console.WriteLine("You chose 2!");
+                break;
+            case 3:
+                // TODO: Be able to filter expenses
+                Console.WriteLine("You chose 3!");
+                break;
+            case 4:
+                // TODO: Calculate total expenses
+                Console.WriteLine("You chose 4!");
+                break;
+            default:
+                Console.WriteLine("You chose 5! Bye bye!");
+                Program.NotExited = false;
+                break;
+        }
+    }
+}
--- Code/Program.cs
+++ Code/Program.cs
@@ -3,31 +3,17 @@
 public static class Program
 {
     private static int _chosenOption;
-    private static bool _notExited = true;
+    internal static bool NotExited = true;
     
     private static void Main()
     {
-        while (_notExited)
+        while (NotExited)
         {
-            ShowMenu();
+            MenuManager.ShowMenu();
             GetInput();
-            ChangeMenu(_chosenOption);
+            MenuManager.ChangeMenu(_chosenOption);
         }
         Environment.Exit(0);
-    }
-
-    private static void ShowMenu()
-    {
-        Console.WriteLine("------------------------------------------------------------");
-        Console.WriteLine("Welcome to the Expense Tracker! Please type ONE option below: ");
-        Console.WriteLine("------------------------------------------------------------");
-        Console.WriteLine("1. Adds expense where you enter the amount, category, description, and date (optional: uses current date if not provided)");
-        Console.WriteLine("2. View Expense");
-        Console.WriteLine("3. Filter Expenses by date or category");
-        Console.WriteLine("4. Calculate Total Expenses");
-        Console.WriteLine("5. Close the program");
-        Console.WriteLine("------------------------------------------------------------");
-        Console.Write("Chosen option (enter a number): ");
     }
     
     // Checks if the userInput is a number and a number between 1 and 5
@@ -50,38 +36,4 @@ public static class Program
             userInput = Console.ReadLine();
         }
     }
-    
-    /// <summary>
-    ///  This method changes the menu based on what number the user inputs.
-    /// </summary>
-    /// <example>If the user selects 1, the console menu changes to show a menu that creates a new expense</example>
-    /// <param name="chosenOption">Based off the global variable _chosenOption</param>
-    private static void ChangeMenu(int chosenOption)
-    {
-        switch (chosenOption)
-        {
-            case 1:
-                // TODO: Implement adding expense
-                Console.WriteLine("You chose 1!");
-                break;
-            case 2:
-                // TODO: Implement the ability to view expense
-                Console.WriteLine("You chose 2!");
-                break;
-            case 3:
-                // TODO: Be able to filter expenses
-                Console.WriteLine("You chose 3!");
-                break;
-            case 4:
-                // TODO: Calculate total expenses
-                Console.WriteLine("You chose 4!");
-                break;
-            default:
-                _notExited = false;
-                Console.WriteLine("You chose 5! Bye bye!");
-                break;
-        }
-    }
-
-
 }
