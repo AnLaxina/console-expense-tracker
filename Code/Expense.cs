4abde6a522ca2a3101aafa9324b3cf767d9af3cd
Anilov Laxina <78817046+AnLaxina@users.noreply.github.com>
2025-03-15 10:53:10 -0700

    Create basic fields for Expense

--- Code/Expense.cs
+++ Code/Expense.cs
@@ -2,5 +2,10 @@
 
 public class Expense
 {
+    private int _id;
+    private DateTime _expenseDate = DateTime.Today;
+    private string _category;
+    private decimal _amount;
+    private string _description;
     
 }
