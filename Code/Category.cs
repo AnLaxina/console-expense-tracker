eef78a6c7e8a75d8209d8a9401819bc0ce5f52c7
Anilov Laxina <78817046+AnLaxina@users.noreply.github.com>
2025-03-15 10:57:58 -0700

    Create a Category enum

---
+++ Code/Category.cs
@@ -0,0 +1,31 @@
+﻿namespace console_expense_tracker.Code;
+
+public enum Category
+{
+    // Essential Expenses
+    Rent,
+    Utilities,
+    Groceries,
+    Transportation,
+    Insurance,
+    // Discretionary Spending
+    Restaurants,
+    Entertainment,
+    Subscriptions,
+    Shopping,
+    // Self-Improvement & Education
+    Books,
+    Courses,
+    Software,
+    Hobbies,
+    // Health & Wellness
+    Medical,
+    Pharmacy,
+    Therapy,
+    Supplements,
+    // Misc.
+    Gifts,
+    Donations,
+    Travel,
+    Emergency
+}
