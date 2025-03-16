1ff024302414d5a80942bf5c8fa41f034a81900b
Anilov Laxina <78817046+AnLaxina@users.noreply.github.com>
2025-03-16 09:47:31 -0700

    Add property for Expense Amount

--- Code/Expense.cs
+++ Code/Expense.cs
@@ -4,8 +4,28 @@ public class Expense
 {
     private int _id;
     private DateTime _expenseDate = DateTime.Today;
-    private string _category;
+    private Category _category;
     private decimal _amount;
     private string _description;
-    
+
+    public Expense(Category category, decimal amount, string description)
+    {
+        _category = category;
+        _amount = amount;
+        _description = description;
+    }
+
+    public decimal Amount
+    {
+        get => _amount;
+        set
+        {
+            if (_amount >= 0)
+            {
+                _amount = value;
+            }
+
+            _amount = 0;
+        }
+    }
 }
