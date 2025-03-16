fd6f2141edf3c1d9a124b96c111994eb001f7f30
Anilov Laxina <78817046+AnLaxina@users.noreply.github.com>
2025-03-16 10:03:16 -0700

    Add more properties for the Expense class

--- Code/Expense.cs
+++ Code/Expense.cs
@@ -2,14 +2,16 @@
 
 public class Expense
 {
-    private int _id;
+    // Make the id a static variable so that it can be accessed by ALL instances of the class
+    private static int _id;
     private DateTime _expenseDate = DateTime.Today;
     private Category _category;
     private decimal _amount;
-    private string _description;
+    private string? _description;
 
     public Expense(Category category, decimal amount, string description)
     {
+        _id += 1;
         _category = category;
         _amount = amount;
         _description = description;
@@ -20,12 +22,37 @@ public class Expense
         get => _amount;
         set
         {
-            if (_amount >= 0)
+            if (value >= 0)
             {
                 _amount = value;
             }
 
             _amount = 0;
+        }
+    }
+
+    public string? Description
+    {
+        get;
+        set;
+    }
+
+    public static int Id
+    {
+        get;
+    }
+
+    public Category Cat
+    {
+        get => _category;
+        set
+        {
+            // Checks to see if what the user enters is a valid enum type
+            if (Enum.IsDefined(value))
+            {
+                _category = value;
+            }
+            
         }
     }
 }
